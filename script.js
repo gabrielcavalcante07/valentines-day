8675f5e774115f8850b88df9f93d337d61369633
gabrielcavalcante07 <bielcavalcante07@gmail.com>
2025-06-07 18:10:49 -0300

    Finalizando a data

--- script.js
+++ script.js
@@ -1,29 +1,56 @@
 window.onload = function () {
-  const startDate = new Date(2024, 10, 17, 0, 0, 0); // 17/11/2024 (mês 10 = novembro)
+  // Data inicial do relacionamento: 17 de novembro de 2024
+  const startDate = new Date(2024, 10, 17, 0, 0, 0);
+
+  function pluralize(value, singular, plural){
+    return `${value} ${value === 1 ? singular : plural}`;
+  }
 
   function updateTime() {
     const now = new Date();
-    let diff = now - startDate;
+    const diff = now - startDate;
 
-    let totalSeconds = Math.floor(diff / 1000);
-    const seconds = totalSeconds % 60;
+    const totalSeconds = Math.floor(diff / 1000);
+    const rawSeconds = totalSeconds % 60;
+    const seconds = rawSeconds === 0 ? 1 : rawSeconds;
 
-    let totalMinutes = Math.floor(totalSeconds / 60);
-    let totalHours = Math.floor(totalMinutes / 60);
-    let totalDays = Math.floor(totalHours / 24);
+    const totalMinutes = Math.floor(totalSeconds / 60);
+    const minutes = totalMinutes % 60;
 
+    const totalHours = Math.floor(totalMinutes / 60);
+    const hours = totalHours % 24;
+
+    const totalDays = Math.floor(totalHours / 24);
     const years = Math.floor(totalDays / 365);
     const months = Math.floor((totalDays % 365) / 30);
-    const days = totalDays % 30;
+    const days = (totalDays % 365) % 30;
 
-    // Atualiza os elementos do HTML
-    document.getElementById("year").textContent = years;
-    document.getElementById("month").textContent = months;
-    document.getElementById("day").textContent = days;
-    document.getElementById("second").textContent = seconds;
+    document.getElementById("month").textContent = pluralize(months, "Mês", "Meses");
+    document.getElementById("day").textContent = pluralize(days, "Dia", "Dias");
+    document.getElementById("hour").textContent = pluralize(hours, "Hora", "Horas");
+    document.getElementById("minute").textContent = pluralize(minutes, "Minuto", "Minutos");
+    document.getElementById("second").textContent = pluralize(seconds, "Segundo", "Segundos");
+
+    const yearCard = document.getElementById("yearCard");
+    const yearText = document.getElementById("yearText");
+
+    if (years >= 1) {
+      yearCard.style.display = "flex";
+      yearText.textContent = years;
+      yearText.textContent = pluralize(years, "Ano", "Anos")
+    } else {
+      yearCard.style.display = "none";
+    }
+
+    document.getElementById(
+      "start-date"
+    ).textContent = `${startDate.toLocaleDateString("pt-BR", {
+      day: "2-digit",
+      month: "long",
+      year: "numeric",
+    })}`;
   }
 
-  // Atualiza a cada segundo
-  updateTime(); // Chama logo que carrega
-  setInterval(updateTime, 1000); // Atualiza a cada 1s
+  updateTime(); // executa assim que a página carrega
+  setInterval(updateTime, 1000); // atualiza a cada 1 segundo
 };
